8ae6f53ac079b184c0089372b1410718c6b0c8cc
tony-akkarakalam <tony.akkarakalam@gmail.com>
2026-06-11 14:28:26 +0530

    Add Google Analytics 4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { Inter, Playfair_Display } from "next/font/google";
+
 import "swiper/css";
 import "swiper/css/pagination";
 import "@/styles/globals.css";
@@ -19,8 +21,24 @@ const playfair = Playfair_Display({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${inter.variable} ${playfair.variable} font-body`}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-RRV6KHBND0"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RRV6KHBND0');
+        `}
+      </Script>
+
+      <div className={`${inter.variable} ${playfair.variable} font-body`}>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
